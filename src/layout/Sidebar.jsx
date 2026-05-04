2383d9cc07dcd0c34db379a306b4874fe77237ba
mandasilemon <amanda10rpl1@gmail.com>
2026-05-05 00:35:10 +0700

    menyelesaikan merge konflik git

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -55,3 +55,25 @@ export default function Sidebar() {
     </nav>
   );
 }
+
+/* import { Link } from "react-router-dom";
+export default function Sidebar() {
+    return (
+        <div style={{
+            width: "220px",
+            height: "100vh",
+            background: "#1e3a8a",
+            color: "white",
+            padding: "20px"
+        }}>
+            <h2>Nusantara</h2>
+
+            <Link to="/" style={{ display: "block", margin: "10px 0", color: "white" }}>
+                Dashboard
+            </Link>
+            <Link to="/data" style={{ display: "block", margin: "10px 0", color: "white" }}>
+                Data Reference
+            </Link>
+        </div>
+    );
+} */
